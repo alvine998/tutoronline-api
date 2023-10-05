7c7fbbf85c9692f39bc359203c57e4cb595e98f3
alvin <alvinecom2018@gmail.com>
2023-10-05 14:07:55 +0700

    --

--- api/models/index.js
+++ api/models/index.js
@@ -1,10 +1,12 @@
 const dbConfig = require("../../config/db.config.js");
+const mysql2 = require("mysql2")
 
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
   operatorsAliases: false,
+  dialectModule: mysql2,
 
   pool: {
     max: dbConfig.pool.max,
