3400c87866b77724e7d4ce80e9ace6970f8b390a
alvin <alvinecom2018@gmail.com>
2023-10-05 14:15:16 +0700

    env setting

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "temank3",
+    HOST: process.env.DB_HOST || "localhost",
+    USER: process.env.DB_USER || "root",
+    PASSWORD: process.env.DB_PASSWORD || "",
+    DB: process.env.DB_NAME || "temank3",
     dialect: "mysql",
     pool: {
         max: 5,
