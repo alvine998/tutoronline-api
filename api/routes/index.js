d422928969547c2e9b2a04a9a531ebc841e50d53
alvin <alvinecom2018@gmail.com>
2024-05-25 15:32:08 +0700

    --

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,10 @@
 const { middlewareHere, middlewarePackageName } = require('../middleware/index.js');
 const multer = require('multer')
+const path = require('path')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, __dirname + '../../../upload/images')
+        cb(null, path.join(__dirname, '../../upload/images'))
     },
     filename: function (req, file, cb) {
         cb(null, `${Date.now()}-${file?.originalname}`)
