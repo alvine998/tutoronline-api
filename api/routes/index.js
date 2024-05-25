c58249b4a96b45fb7a1b100b6c63bcbcbba66dff
alvin <alvinecom2018@gmail.com>
2024-05-25 15:01:36 +0700

    --

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'upload/images')
+        cb(null, '/var/www/tokotitoh_api/upload/images')
     },
     filename: function (req, file, cb) {
         cb(null, `${file?.originalname}`)
