44bfed31c119ca5800c93018b59bf7f73ba93179
alvin <alvinecom2018@gmail.com>
2024-05-25 15:33:54 +0700

    --

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../../upload/images'))
+        cb(null, path.join(__dirname, '../../upload'))
     },
     filename: function (req, file, cb) {
         cb(null, `${Date.now()}-${file?.originalname}`)
