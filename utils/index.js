cf5392e2f49adb11818e32b30f355c9a2f34160e
alvin <alvinecom2018@gmail.com>
2024-06-21 17:33:51 +0700

    --

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,4 @@
-export function generateRandomSixDigitNumber() {
+exports.generateRandomSixDigitNumber = () => {
     const min = 100000;
     const max = 999999;
     return Math.floor(Math.random() * (max - min + 1)) + min;
